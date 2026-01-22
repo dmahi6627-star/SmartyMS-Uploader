33466a463168ed36882fdb00cbc821a7004bf109
nawaabbro6-png <nawaabbro6@gmail.com>
2026-01-22 16:55:25 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -554,7 +554,7 @@ async def txt_handler(bot: Client, m: Message):
             try:  
         
                 cc = f'**📹 VID_ID: {str(count).zfill(3)}.\n\n📒 Title: {name1} {res}.mkv\n\n<pre><code>📚 Batch Name: {b_name}</code></pre>\n\n📥 Extracted By𓆩♛𓆪 : {CR}\n\n**∘𒆜━━━❀💛𝐌𝐒💚❀━━━𒆜∘**'
-                cc1 = f'**💾 PDF_ID: {str(count).zfill(3)}.\n\nTitle: {name1} STUDENTS💛.pdf\n\n📚 Batch Name: {b_name}\n\n📥 Extracted By♠ : {CR}\n\n**∘𒆜━━━❀💛𝐌𝐒💚❀━━━𒆜∘**'
+                cc1 = f'**🗃️ PDF_ID: {str(count).zfill(3)}.\n\n📒 Title: {name1} .pdf\n\n<pre><code>📚 Batch Name: {b_name}</code></pre>\n\n📥 Extracted Extracted By𓆩♛𓆪 : {CR}\n\n**∘𒆜━━━❀💛𝐌𝐒💚❀━━━𒆜∘**'
                     
                 
                 if "drive" in url:
